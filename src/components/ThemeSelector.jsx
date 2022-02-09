692c19b3068a6d44ce04710d057f396532d53d86
Dariusz Okonski <darek200180@gmail.com>
2022-02-09 15:52:19 +0000

    finish dark theme mode

--- src/components/ThemeSelector.jsx
+++ src/components/ThemeSelector.jsx
@@ -7,12 +7,21 @@ import { useTheme } from './../hooks/useTheme';
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-    const {changeColor} = useTheme();
+    const {changeColor, changeMode, mode} = useTheme();
   
+    const toggleMode = () => {
+        changeMode(mode === 'dark' ? 'light' : 'dark')
+    }
+
     return (
       <div className='theme-selector'>
           <div className="mode-toggle">
-              <img src={modeIcon} alt="" />
+            <img
+                onClick={toggleMode}
+                src={modeIcon} 
+                alt="dark/light toggle icon"
+                style={{ filter: mode === 'dark' ? 'invert(100%)' : 'invert(20%)'}}
+            />
           </div>
 
           <div className="theme-buttons">
