7fd071ab466b625abb76f5be7656686ac162cdba
Dariusz Okonski <darek200180@gmail.com>
2022-02-10 16:44:02 +0000

    fetch main view from database

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,48 @@
 import RecipeList from '../../components/RecipeList';
-import useFetch from './../../hooks/useFetch';
+import { projectFirestore } from '../../firebase/config';
+// import useFetch from './../../hooks/useFetch';
 import './Home.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export default function Home() {
-    const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+    // const { data, isPending, error } = useFetch('http://localhost:3000/recipes')
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(false);
+    const [error, setError] = useState(false);
 
+    useEffect(() => {
+        setIsPending(true);
 
-  return (
-      <div className='home'>
-          { error && <p className='error'>{error}</p>}
-          { isPending && <p className='loading'>Loading...</p>}
-          { data && <RecipeList recipes={data} /> }
-      </div>
-  );
+        projectFirestore.collection('recipes')
+            .get()
+            .then((snapshot) => {
+                if(snapshot.empty) {
+                    setError('No recipes to load');
+                    isPending(false);
+                } else {
+                    let results = [];
+                    snapshot.docs.forEach(doc => {
+                        results.push({id: doc.id, ...doc.data()})
+                    })
+
+                    setData(results);
+                    setIsPending(false);
+                }
+            })
+            .catch(err => {
+                setError(err.message);
+                setIsPending(false);
+            })
+
+    }, [])
+    
+
+    return (
+        <div className='home'>
+            { error && <p className='error'>{error}</p>}
+            { isPending && <p className='loading'>Loading...</p>}
+            { data && <RecipeList recipes={data} /> }
+        </div>
+    );
 }
