4abddbf661573b042e1f2b574bd91300684ceeea
Dariusz Okonski <darek200180@gmail.com>
2022-02-11 16:39:35 +0000

    add functionality to remove element

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { useTheme } from '../hooks/useTheme';
 import './RecipeList.css';
+import Trashcan from '../assets/trashcan.svg';
+import { projectFirestore } from '../firebase/config';
 
 export default function RecipeList({ recipes }) {
     
@@ -9,6 +11,11 @@ export default function RecipeList({ recipes }) {
     if(recipes.length === 0) {
         return <div className='error'>No recipes to load...</div>
     }
+
+    const handleClick = (id) => {
+        projectFirestore.collection('recipes').doc(id).delete();
+    }
+    
 
     return (
         <div className='recipe-list'>
@@ -19,6 +26,12 @@ export default function RecipeList({ recipes }) {
                         <p>{recipe.cookingTime} to make</p>
                         <div>{recipe.method.substring(0, 100)}...</div>
                         <Link to={`/recipes/${recipe.id}`}>Cook this...</Link>
+                        <img
+                            className='delete' 
+                            src={Trashcan} 
+                            alt="delete"
+                            onClick={() => handleClick(recipe.id)} 
+                        />
                     </div>
                 ))
             }
